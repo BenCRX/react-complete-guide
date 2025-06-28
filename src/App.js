bc00109bcb2cb436d6f6ee42df4dcc712a9dade6
benCRX <ben.codecademy@gmail.com>
2025-06-28 14:14:34 +0200

    59 & 60 Finished

--- src/App.js
+++ src/App.js
@@ -5,28 +5,34 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { name: "Ben", age: 37 },
-      { name: "John", age: 42 },
-      { name: "Rita", age: 12 },
+      { id: "98745", name: "Ben", age: 37 },
+      { id: "32459", name: "John", age: 42 },
+      { id: "97854", name: "Rita", age: 12 },
     ],
     showPersons: false,
   };
 
   handleDeletePerson = (index) => {
+    //Create a copy of the state array otherwise since persons is a pointer it will modify the state and this is not something you want to do. Therefor, using the .slice() method or the spread operator [...array]
     //const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(index, 1);
     this.setState({ persons: persons });
   };
 
-  handleOnChangeName = (event) => {
-    this.setState({
-      persons: [
-        { name: "Ben", age: 37 },
-        { name: event.target.value, age: 42 },
-        { name: "Rita", age: 12 },
-      ],
+  handleOnChangeName = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
     });
+
+    const tempPerson = { ...this.state.persons[personIndex] };
+
+    tempPerson.name = event.target.value;
+
+    const tempPersons = [ ...this.state.persons ];
+    tempPersons[personIndex] = tempPerson;
+
+    this.setState({ persons: tempPersons });
   };
 
   handleTogglePersons = () => {
@@ -51,9 +57,11 @@ class App extends Component {
           {this.state.persons.map((person, index) => {
             return (
               <Person
+                key={person.id}
                 name={person.name}
                 age={person.age}
                 onClick={() => this.handleDeletePerson(index)}
+                onChange={(event) => this.handleOnChangeName(event, person.id)}
               />
             );
           })}
