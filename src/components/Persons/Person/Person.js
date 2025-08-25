35141ce2d5406af06a29d3e3da2e4b5bfbe669df
benCRX <ben.codecademy@gmail.com>
2025-08-25 17:24:54 +0200

    102 In Progress Example - Rendering Adjacent JSX Elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,19 +6,18 @@ class Person extends Component
   render()
   {
     console.log("[Person.js] rendering...");
-    return (
-      <div className={style.Person}>
-        <p onClick={this.props.onClick}>
-          I'm {this.props.name} and I am {this.props.age} years old
-        </p>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          onChange={this.props.onChange}
-          value={this.props.name}
-        />
-      </div>
-    );
+
+    // You should usually have a parent element when you return some JSX but it is possible to return an array as long as elements of the array have a proper key
+    return [
+      <p key="i1" onClick={this.props.onClick}>I'm {this.props.name} and I am {this.props.age} years old</p>,
+      <p key="i2">{this.props.children}</p>,
+      <input
+        key="i3"
+        type="text"
+        onChange={this.props.onChange}
+        value={this.props.name}
+      />,
+    ];
   }
 }
 
