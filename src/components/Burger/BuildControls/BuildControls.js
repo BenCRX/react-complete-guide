09763310975d193a8bafdf0e9523c318828f8e1c
benCRX <ben.codecademy@gmail.com>
2025-09-02 18:23:08 +0200

    131 Finished - Adding the Build Control Component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./BuildControls.module.css";
+
+const BuildControls = (props) =>
+{
+    return (
+        <div className={style.BuildControls}>BuildControls</div>
+    )
+}
+
+export default BuildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,16 @@ import style from '../Burger/Burger.module.css'
 
 const Burger = (props) =>
 {
-    let transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => 
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => 
     {
-        return [...Array(props.ingredients[ingredientKey])].map((_, index) => 
+        return [...Array(props.ingredients[ingredientKey])]
+        .map((_, index) => 
         {
             return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
         });
-    }).reduce((arr, el) => arr.concat(el), []);
+    })
+    .reduce((arr, el) => arr.concat(el), []);
 
     if(transformedIngredients.length === 0){
         transformedIngredients = <p>Please start adding ingredients</p>
