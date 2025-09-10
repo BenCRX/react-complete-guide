a2a83c2eb239e0d88fc5f815e103ea02e9e25d6f
benCRX <ben.codecademy@gmail.com>
2025-09-10 15:13:02 +0200

    143 Finished - Adding a Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 import style from "./Layout.module.css"
-//import Auxiliary from "../../hoc/Auxiliary";
 
 const Layout = (props) =>
 {
     return (
         <React.Fragment>
-            <div>Toolbar, Sidedrawer, Backdrop</div>
-
+            <Toolbar />
             <main className={style.Content}>
                 {props.children}
             </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./Toolbar.module.css"
+
+const Toolbar = (props) => {
+  return (
+    <header className={style.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>...</nav>
+    </header>
+  )
+};
+
+export default Toolbar;
